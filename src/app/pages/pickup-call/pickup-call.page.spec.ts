ad7e19ad97759f7f667692eaf978f40ca0b1d10a
Veronika Sosolikova <v_sosolikova@utb.cz>
2023-01-23 22:10:36 +0100

    Connection from Pickup-call to Home

--- src/app/pages/pickup-call/pickup-call.page.spec.ts
+++ src/app/pages/pickup-call/pickup-call.page.spec.ts
@@ -1,19 +1,26 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 import { PickupCallPage } from './pickup-call.page';
 
 describe('PickupCallPage', () => {
   let component: PickupCallPage;
   let fixture: ComponentFixture<PickupCallPage>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PickupCallPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        IonicModule.forRoot(),
+        AppRoutingModule
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(PickupCallPage);
+    router = TestBed.get(Router);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
@@ -21,4 +28,9 @@ describe('PickupCallPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should go to home on create new pickup call',() =>{
+    spyOn(router, 'navigate');
+    component.newPickupCall()
+    expect(router.navigate).toHaveBeenCalledWith(['home']);
+  })
 });
