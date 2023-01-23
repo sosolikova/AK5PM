1799fc3f02ace03900b55fdcf1f9b4bbc0f8037d
Veronika Sosolikova <v_sosolikova@utb.cz>
2023-01-23 21:41:21 +0100

    Connection from home to pickup calls

--- src/app/pages/home/home.page.spec.ts
+++ src/app/pages/home/home.page.spec.ts
@@ -1,19 +1,26 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 import { HomePage } from './home.page';
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HomePage ],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        IonicModule.forRoot(),
+        AppRoutingModule
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
+    router = TestBed.get(Router);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
@@ -21,4 +28,18 @@ describe('HomePage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should go to pickup-calls on see all',() => {
+    spyOn(router,'navigate');
+    component.goToPickupCalls();
+    expect(router.navigate).toHaveBeenCalledWith(['pickup-calls']);
+  })
+
+  it('should go to pickup call on create pickup call',() => {
+    spyOn(router,'navigate');
+    component.newPickupCall();
+
+    expect(router.navigate).toHaveBeenCalledWith(['pickup-call']);
+  })
+
 });
